ca7ec56315b5cab4fe4c47fc0695314b471ebd22
akundurt <akundurt@GDCPHXDT118456.igategegdc.com>
2018-02-26 20:24:02 +0530

    this is my first commit

---
+++ ThirdSampleExample/src/test/java/ThirdSample/ThirdSampleExample/Example1.java
@@ -0,0 +1,13 @@
+package ThirdSample.ThirdSampleExample;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("This is First push to git");
+	}
+
+}
